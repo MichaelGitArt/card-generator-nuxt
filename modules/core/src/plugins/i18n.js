9cfcd7fe656d811d2f1162494c17bde1c95bb4df
Michael <michaelgitart@gmail.com>
2021-01-12 02:18:16 +0200

    fix: eslint fixing

--- modules/core/src/plugins/i18n.js
+++ modules/core/src/plugins/i18n.js
@@ -6,40 +6,40 @@ export default async ({
   let language = DEFAULT_LANGUAGE;
 
   const setI18nLanguage = ({
-      lang, message,
+    lang, message,
   }) => {
-      app.i18n.setLocaleMessage(lang, message);
-      loadedLanguages.push(lang);
-      app.i18n.locale = lang;
+    app.i18n.setLocaleMessage(lang, message);
+    loadedLanguages.push(lang);
+    app.i18n.locale = lang;
   };
   const loadLanguageAsync = async (lang) => {
-      if (app.i18n.locale === lang || loadedLanguages.includes(lang)) {
-          app.i18n.locale = lang;
-      } else {
-          try {
-              if (!loadedLanguages.length) {
-                  const defaultMessage = await import(`@/.nuxt/locales/${DEFAULT_LANGUAGE}`);
+    if (app.i18n.locale === lang || loadedLanguages.includes(lang)) {
+      app.i18n.locale = lang;
+    } else {
+      try {
+        if (!loadedLanguages.length) {
+          const defaultMessage = await import(`@/.nuxt/locales/${DEFAULT_LANGUAGE}`);
 
-                  setI18nLanguage({
-                      lang: DEFAULT_LANGUAGE,
-                      message: defaultMessage.default,
-                  });
-              }
-              const message = await import(`@/.nuxt/locales/${lang}`);
+          setI18nLanguage({
+            lang: DEFAULT_LANGUAGE,
+            message: defaultMessage.default,
+          });
+        }
+        const message = await import(`@/.nuxt/locales/${lang}`);
 
-              setI18nLanguage({
-                  lang,
-                  message: message.default,
-              });
-          } catch (e) {
-              app.i18n.locale = DEFAULT_LANGUAGE;
-          }
+        setI18nLanguage({
+          lang,
+          message: message.default,
+        });
+      } catch (e) {
+        app.i18n.locale = DEFAULT_LANGUAGE;
       }
+    }
   };
 
   loadLanguageAsync(language);
 
   inject('setInterfaceLanguage', async (lang) => {
-      await loadLanguageAsync(lang);
+    await loadLanguageAsync(lang);
   });
 };
