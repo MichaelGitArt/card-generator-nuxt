169882f24513bbfb0b4324d7ddd043b2da43a491
Michael <michaelgitart@gmail.com>
2021-01-15 18:47:29 +0200

    feat: update ui scss

--- modules/ui/src/config/index.js
+++ modules/ui/src/config/index.js
@@ -6,6 +6,7 @@ export default {
   css: [
     './assets/scss/reset.scss',
     './assets/scss/fonts.scss',
+    './assets/scss/global.scss',
   ],
   styleResources: {
     scss: './assets/scss/main.scss',
